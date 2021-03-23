0ddbc7657b05706f03c0a8a363d7dc0d9d001858
lsfans <lsfans@qq.com>
2021-03-24 00:55:04 +0800

    show the method of simulating fixed point value in this example

--- src/main/scala/projects/FTN/ComplexMultiplier.scala
+++ src/main/scala/projects/FTN/ComplexMultiplier.scala
@@ -1,7 +1,9 @@
 package projects.FTN
 
+import breeze.numerics.{ceil, floor}
 import spinal.core._
 import spinal.lib._
+import spinal.core.sim._
 
 // R + jI = (X + jY) × (C + jS), where C and S are constants while X and Y are inputs
 /*
@@ -16,7 +18,8 @@ class StaticComplexMultiplier(C: Double, S: Double) extends Component {
   println(data.minValue)
   println(data.resolution)
 
-  val io = new Bundle{
+  val io = new Bundle {
+//    val X, Y = in(data)
     val X, Y = in(data)
     val R, I = out(data)
   }
@@ -29,7 +32,7 @@ class StaticComplexMultiplier(C: Double, S: Double) extends Component {
   val Z = Cfix * E
 
   io.R := ((Cfix - Sfix) * io.Y + Z).truncated
-  io.I := ((Cfix + Sfix) * io.X + Z).truncated
+  io.I := ((Cfix + Sfix) * io.X - Z).truncated
 }
 
 object StaticComplexMultiplier {
@@ -37,4 +40,24 @@ object StaticComplexMultiplier {
     SpinalConfig(mode = SystemVerilog)
       .generate(new StaticComplexMultiplier(1.25, 2.25))
   }
+}
+
+object testStaticComplexMultiplier {
+
+  def Double2Fix(value: Double) = floor(value * (1 << 8)).toInt
+
+  def main(args: Array[String]): Unit = {
+    SimConfig.withWave.compile(new StaticComplexMultiplier(1.25, 2.25))
+      .doSimUntilVoid { dut =>
+
+        dut.io.X.raw  #= Double2Fix(4.0)
+        dut.io.Y.raw  #= 2048
+        sleep(3)
+        dut.io.X.raw #= 2048
+        dut.io.Y.raw #= 4096
+        sleep(3)
+        simSuccess()
+      }
+  }
+
 }
