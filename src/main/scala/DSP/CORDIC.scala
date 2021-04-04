61bb6faea430fdce948d1c897cb81a38748aa19b
lsfans <lsfans@qq.com>
2021-04-04 20:23:42 +0800

    fix the unconneted paths, fix the math problem on hyperbolic mode, finish verification framework, 3/6 modes have been verified

--- src/main/scala/DSP/CORDIC.scala
+++ src/main/scala/DSP/CORDIC.scala
@@ -5,6 +5,8 @@ import breeze.numerics._
 import spinal.core._
 import spinal.lib._
 
+import scala.collection.mutable.ListBuffer
+
 sealed trait RotationMode
 
 object RotationMode {
@@ -17,19 +19,27 @@ object RotationMode {
 
 sealed trait AlgebricMode
 
+object AlgebricMode {
+
+  case object CIRCULAR extends AlgebricMode
+
+  case object LINEAR extends AlgebricMode
+
+  case object HYPERBOLIC extends AlgebricMode
+
+}
+
 sealed trait CORDICArch
 
-object CIRCULAR extends AlgebricMode
+object CORDICArch {
 
-object LINEAR extends AlgebricMode
+  case object PIPELINED extends CORDICArch
 
-object HYPERBOLIC extends AlgebricMode
+}
 
-
-object PIPELINED extends CORDICArch
-
+import DSP.AlgebricMode._
+import DSP.CORDICArch._
 import DSP.RotationMode._
-
 
 case class CordicData() extends Bundle {
   val x = data
@@ -37,12 +47,19 @@ case class CordicData() extends Bundle {
   val z = data
 }
 
-class CORDIC(mode: RotationMode = ROTATION, arch: CORDICArch = PIPELINED, iterations: Int = 20) extends Component {
+class CORDIC(rotationMode: RotationMode = ROTATION,
+             algebricMode: AlgebricMode = CIRCULAR,
+             arch: CORDICArch = PIPELINED,
+             iterations: Int = 43) extends Component {
 
   val input = slave Flow CordicData()
   val output = master Flow CordicData()
 
-  val outputs = cordic(input.payload.x, input.payload.y, input.payload.z, iterations, CIRCULAR, VECTORING)
+  val outputs = cordic(
+    input.payload.x, input.payload.y, input.payload.z,
+    rotationMode, algebricMode,
+    iterations)
+
   output.payload.x := outputs._1.truncated
   output.payload.y := outputs._2.truncated
   output.payload.z := outputs._3.truncated
@@ -53,22 +70,21 @@ class CORDIC(mode: RotationMode = ROTATION, arch: CORDICArch = PIPELINED, iterat
 // TODO: algebricMode: Bits, rotationMode: Bool, pipelined: Int
 object CORDIC {
   def cordic(inputX: SFix, inputY: SFix, inputZ: SFix,
-             iterations: Int, algebricMode: AlgebricMode, rotationMode: RotationMode) = {
-
-    //    val widthInX = inputX.getBitsWidth
-    //    val widthInY = inputY.getBitsWidth
-    //    val widthIn = if (widthInX > widthInY) widthInX else widthInY
-    //    val widthInZ = inputZ.getBitsWidth
-    //    val widthOut = widthIn + iterations
+             rotationMode: RotationMode, algebricMode: AlgebricMode,
+             iterations: Int) = {
 
     def magnitudeDataType(i: Int) = SFix(peak = inputX.maxExp exp, width = (inputX.bitCount + i) bits)
 
     def phaseDataType = SFix(peak = inputZ.maxExp exp, width = (inputZ.bitCount) bits)
 
-    val circularCoeffcients = (0 until iterations).map(i => SF(getPhase(i)(CIRCULAR), inputZ.maxExp exp, inputZ.bitCount bits)).toArray
-    circularCoeffcients.foreach(_.setName("coeffs"))
-    println((0 until iterations).map(i => getPhase(i)(CIRCULAR)).mkString(" "))
-    //    val circularCoefficientTable = Mem(SFix, circularCoeffcients)
+    val phaseCoeffcients = (0 until iterations).map(i => SF(getPhase(i)(algebricMode), inputZ.maxExp exp, inputZ.bitCount bits)).toArray
+    val shiftingCoeffs = if (algebricMode == HYPERBOLIC) getHyperbolicSequence(iterations) else (0 until iterations)
+    phaseCoeffcients.foreach(_.setName("coeffs"))
+    val scaleComplement = SF(getScaleComplement(iterations)(algebricMode), inputX.maxExp exp, inputX.bitCount bits)
+    scaleComplement.setName("scaleComplement")
+    println("phase: " + (0 until iterations).map(i => getPhase(i)(algebricMode)).mkString(" "))
+    println("shift: " + shiftingCoeffs.mkString(" "))
+    println("scaleComplement: " + getScaleComplement(iterations)(algebricMode).toString)
 
     val regsX = (0 until iterations).map(i => Reg(magnitudeDataType(16))).toList
     regsX.foreach(_.setName("regsX"))
@@ -77,15 +93,20 @@ object CORDIC {
     val regsZ = (0 until iterations).map(i => Reg(phaseDataType)).toList
     regsZ.foreach(_.setName("regsZ"))
 
-    val shiftedX = (inputX :: regsX.dropRight(1)).zipWithIndex.map { case (regX, i) => regX >> i }
+
+    //    val shiftedX = (inputX :: regsX.dropRight(1)).zipWithIndex.map { case (regX, i) => regX >> i }
+    //    shiftedX.foreach(_.setName("shiftedX"))
+    //    val shiftedY = (inputY :: regsY.dropRight(1)).zipWithIndex.map { case (regY, i) => regY >> i }
+    //    shiftedY.foreach(_.setName("shiftedY"))
+    val shiftedX = (inputX :: regsX.dropRight(1)).zip(shiftingCoeffs).map { case (regX, i) => regX >> i }
     shiftedX.foreach(_.setName("shiftedX"))
-    val shiftedY = (inputY :: regsY.dropRight(1)).zipWithIndex.map { case (regY, i) => regY >> i }
+    val shiftedY = (inputY :: regsY.dropRight(1)).zip(shiftingCoeffs).map { case (regY, i) => regY >> i }
     shiftedY.foreach(_.setName("shiftedY"))
 
     val counterClockWises =
       rotationMode match {
         case VECTORING => (inputY :: regsY.dropRight(1)).map(_.asBits.msb) // Y < 0
-        case ROTATION => regsZ.map(~_.asBits.msb) // Z > 0
+        case ROTATION => (inputZ :: regsZ.dropRight(1)).map(~_.asBits.msb) // Z > 0
       }
     //    counterClockWises.foreach(_.setName("counterClockwise"))
 
@@ -103,22 +124,37 @@ object CORDIC {
         }
       }
 
-    (inputZ :: regsZ.dropRight(1)).zip(regsZ).zip(counterClockWises.zip(circularCoeffcients))
+    (inputZ :: regsZ.dropRight(1)).zip(regsZ).zip(counterClockWises.zip(phaseCoeffcients))
       .foreach { case ((prev, cur), (cond, coeff)) =>
         cur := Mux(cond, prev - coeff, prev + coeff).truncated
       }
 
-    val scaleComplement = SF(getScaleComplement(iterations)(CIRCULAR), inputX.maxExp exp, inputX.bitCount bits)
-    scaleComplement.setName("scaleComplement")
-
     (regsX.last * scaleComplement, regsY.last * scaleComplement, regsZ.last)
+  }
+
+  def getHyperbolicSequence(iterations: Int) = { // length = iterations
+
+    require(iterations >= 1)
+
+    var currentSpecial = 1
+    var i = iterations - 1
+    val result = ListBuffer(1)
+    while (i > 0) {
+      if (result.last == (currentSpecial * 3 + 1)) {
+        result += result.last
+        currentSpecial = currentSpecial * 3 + 1
+      }
+      else result += (result.last + 1)
+      i -= 1
+    }
+    result.toIndexedSeq
   }
 
   def getPhase(iteration: Int)(implicit algebricMode: AlgebricMode) = {
     algebricMode match {
       case CIRCULAR => atan(pow(2.0, -iteration))
       case LINEAR => -pow(2.0, -iteration)
-      case HYPERBOLIC => tanh(pow(2.0, -(iteration + 1)))
+      case HYPERBOLIC => atanh(pow(2.0, -getHyperbolicSequence(iteration + 1).last))
     }
   }
 
@@ -127,14 +163,21 @@ object CORDIC {
 
     algebricMode match {
       case CIRCULAR => (0 until iterations).map(i => cos(getPhase(i))).product
-      case HYPERBOLIC => (0 until iterations).map(i => sqrt(1 - pow(2.0, -2 * (i + 1)))).product
+      case LINEAR => 1.0
+      case HYPERBOLIC => 1.0 / (1 to iterations)
+        .map(i => getHyperbolicSequence(i).last)
+        .map(i => sqrt(1 - pow(2.0, -2 * i))).product
     }
   }
 
   def main(args: Array[String]): Unit = {
     implicit val mode = CIRCULAR
-    (0 until 10).foreach(i => println(getPhase(i)))
-    (1 until 10).foreach(i => println(getScaleComplement(i)))
+    //    (0 until 10).foreach(i => println(getPhase(i)))
+    //    (1 until 10).foreach(i => println(getScaleComplement(i)))
+    (1 until 10).foreach(i => println(getScaleComplement(i)(HYPERBOLIC)))
+    println(getHyperbolicSequence(10).mkString(" "))
+    println(getHyperbolicSequence(100).mkString(" "))
+
     //    VivadoFlow(design = new CORDIC(), topModuleName = "CORDIC", workspacePath = "output/CORIDC", force = true).doit()
   }
 }
--- src/main/scala/DSP/testCORDIC.scala
+++ src/main/scala/DSP/testCORDIC.scala
@@ -1,16 +1,22 @@
 package DSP
 
-import breeze.linalg.DenseVector
-import breeze.math.Complex
+import DSP.AlgebricMode._
+import DSP.RotationMode._
 import breeze.numerics.constants.Pi
-import breeze.numerics.{cos, sin}
+import breeze.numerics._
 import spinal.core._
 import spinal.core.sim._
 
 import scala.util.Random
 
-class testCORDIC() extends CORDIC() with DSPSim {
-  override type TestCase = Array[Double]
+case class cordicTestCase(x: Double, y: Double, z: Double) {
+  override def toString: String = s"x: $x, y: $y, z: ${z / Pi * 180.0} degree"
+}
+
+class testCORDIC(rotationMode: RotationMode, algebricMode: AlgebricMode)
+  extends CORDIC(rotationMode = rotationMode, algebricMode = algebricMode)
+    with DSPSim {
+  override type TestCase = cordicTestCase
   override type ResultType = Array[Double]
 
   override def simInit(): Unit = {
@@ -29,12 +35,12 @@ class testCORDIC() extends CORDIC() with DSPSim {
       while (true) {
         if (testCases.nonEmpty) {
           val testCase = testCases.dequeue()
-          println("test: " + testCase.mkString(" "))
+          println("test: " + testCase.toString)
           referenceModel(testCase)
           input.valid #= true
-          input.payload.x.raw #= Double2Fix(testCase(0))
-          input.payload.y.raw #= Double2Fix(testCase(1))
-          input.payload.z.raw #= Double2Fix(testCase(2))
+          input.payload.x.raw #= Double2Fix(testCase.x)
+          input.payload.y.raw #= Double2Fix(testCase.y)
+          input.payload.z.raw #= Double2Fix(testCase.z)
           clockDomain.waitSampling()
           input.valid #= false
         }
@@ -44,7 +50,49 @@ class testCORDIC() extends CORDIC() with DSPSim {
   }
 
   override def referenceModel(testCase: TestCase): Unit = {
-    val golden = Array(0.0, 0.0, 0.0)
+
+    val x = testCase.x
+    val y = testCase.y
+    val z = testCase.z
+    var xResult = 0.0
+    var yResult = 0.0
+    var zResult = 0.0
+    //    val scaleFactor = 1.0 / CORDIC.getScaleComplement(20)(algebricMode)
+
+    (rotationMode, algebricMode) match {
+      case (ROTATION, CIRCULAR) => {
+        xResult = x * cos(z) - y * sin(z)
+        yResult = y * cos(z) + x * sin(z)
+        zResult = 0.0
+      }
+      case (ROTATION, LINEAR) => {
+        xResult = x
+        yResult = y + x * z
+        zResult = 0.0
+      }
+      case (ROTATION, HYPERBOLIC) => {
+        xResult = x * cosh(z) - y * sinh(z)
+        yResult = y * cosh(z) + x * sinh(z)
+        zResult = 0.0
+      }
+      case (VECTORING, CIRCULAR) => {
+        xResult = sqrt(x * x + y * y)
+        zResult = z + atan(y / x)
+        yResult = 0.0
+      }
+      case (VECTORING, LINEAR) => {
+        xResult = x
+        zResult = z + y / x
+        yResult = 0.0
+      }
+      case (VECTORING, HYPERBOLIC) => {
+        xResult = sqrt(x * x - y * y)
+        zResult = z + atanh(y / x)
+        yResult = 0.0
+      }
+    }
+
+    val golden = Array(xResult, yResult, zResult)
     refResults.enqueue(golden)
   }
 
@@ -62,18 +110,14 @@ class testCORDIC() extends CORDIC() with DSPSim {
 
   override def scoreBoard(): Unit = {
 
-    def same(a: Double, b: Double) = scala.math.abs(a - b) / ((a + b) / 2) < 0.01 || scala.math.abs(a - b) < 0.1
-
-    def sameVector(v1: DenseVector[Complex], v2: DenseVector[Complex]) =
-      v1.toArray.zip(v2.toArray).forall { case (c1, c2) => same(c1.real, c2.real) && same(c1.imag, c2.imag) }
-
     val score = fork {
       while (true) {
         if (refResults.nonEmpty && dutResults.nonEmpty) {
           val refResult = refResults.dequeue()
           val dutResult = dutResults.dequeue()
-          println(s"result: ${dutResult.mkString(" ")}")
-          assert(true, s"$refResult \n $dutResult")
+          val same = refResult.zip(dutResult).map { case (ref, dut) => abs(ref - dut) }.forall(_ < 1E-1)
+          assert(same, s"\n result: ${dutResult.mkString(" ")} \n golden: ${refResult.mkString(" ")}")
+          //                    println(s"\n result: ${dutResult.mkString(" ")} \n golden: ${refResult.mkString(" ")}")
         }
         clockDomain.waitSampling()
       }
@@ -84,29 +128,58 @@ class testCORDIC() extends CORDIC() with DSPSim {
 object testCORDIC {
   private val r = Random
 
-  //    def randomCase(length: Int) = (for (elem <- (0 until 2 * length)) yield (r.nextDouble() * scala.math.pow(2, naturalWidth / 2))).toArray
-  private def randomCase() = {
-    val phase = (r.nextDouble() - 0.5) * Pi
-    val x = cos(phase)
-    val y = sin(phase)
-    Array(x, y, phase)
+  private def randomCase(rotationMode: RotationMode, algebricMode: AlgebricMode) = {
+    var x = 0.0
+    var y = 0.0
+    var z = 0.0
+
+    (rotationMode, algebricMode) match {
+      case (VECTORING, CIRCULAR) => {
+        val rand = (r.nextDouble() - 0.5) * Pi
+        x = cos(rand)
+        y = sin(rand)
+        z = 0.0
+      }
+      case (VECTORING, HYPERBOLIC) => {
+        x = r.nextDouble() * 16
+        y = x * r.nextDouble() * 0.8
+        z = 0.0
+      }
+      case (ROTATION, CIRCULAR) => {
+        x = 1.0
+        y = 0.0
+        z = (r.nextDouble() - 0.5) * Pi
+      }
+    }
+
+    cordicTestCase(x, y, z)
   }
 
-  def randomSim(): Unit = {
-    val dut = SimConfig.withWave.compile(new testCORDIC())
+  def randomSim(rotationMode: RotationMode, algebricMode: AlgebricMode): Unit = {
+
+    val dut = SimConfig.withWave.compile(new testCORDIC(rotationMode, algebricMode))
     dut.doSim { dut =>
       dut.sim()
-      //      for (i <- 0 until 10) dut.insertTestCase(randomCase())
-      dut.insertTestCase(Array(0.707, 0.707, 0))
+      for (i <- 0 until 10) dut.insertTestCase(randomCase(rotationMode, algebricMode))
       dut.simDone()
     }
-    print(Console.GREEN)
-    println(s"CORDIC, PASS")
-    print(Console.BLACK)
   }
 
   def main(args: Array[String]): Unit = {
-    randomSim()
+    randomSim(VECTORING, CIRCULAR)
+    print(Console.GREEN)
+    println(s"CORDIC VECTORING + CIRCULAR, PASS")
+    print(Console.BLACK)
+
+    randomSim(VECTORING, HYPERBOLIC)
+    print(Console.GREEN)
+    println(s"CORDIC VECTORING + HYPERBOLIC, PASS")
+    print(Console.BLACK)
+
+    randomSim(ROTATION, CIRCULAR)
+    print(Console.GREEN)
+    println(s"CORDIC ROTATION + CIRCULAR, PASS")
+    print(Console.BLACK)
   }
 }
 
