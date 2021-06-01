190fbd1f396080697d3bca816d186881c6390678
ltr <litianr@mail2.sysu.edu.cn>
2021-06-01 13:07:13 +0800

    comment the Interleave.m

--- src/main/scala/FTN/Matlab/Bits2QAM.m
+++ src/main/scala/FTN/Matlab/Bits2QAM.m
@@ -2,7 +2,6 @@ function QAMSymbols = Bits2QAM(bits)
     save './data/bits' bits;
     convCodedBits = Convenc(bits); % 卷积编码
     save './data/convCodedBits' convCodedBits;
-    % interleavedBits = Interleave(convCodedBits); % 交织
     interleavedBits = Interleaver(convCodedBits, 1); % 交织
     save './data/interleavedBits' interleavedBits;
     QAMSymbols = DynamicQammod(interleavedBits); % (动态)QAM映射
--- src/main/scala/FTN/Matlab/QAM2Bits.m
+++ src/main/scala/FTN/Matlab/QAM2Bits.m
@@ -1,7 +1,6 @@
 function recvBits = QAM2Bits(QAMSymbols)
     demodulated = DynamicQamdemod(QAMSymbols); % (动态)QAM解映射
     save './data/demodulated' demodulated
-    % deinterleaved = Deinterleave(demodulated); % 解交织
     deinterleaved = Interleaver(demodulated, 0); % 解交织
     save './data/deinterleaved' deinterleaved
     recvBits = Vitdec(deinterleaved); % 维特比译码
