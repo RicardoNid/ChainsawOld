166b228ae0228fa0a499ddb1a827c782a7ceec68
ltr <litianr@mail2.sysu.edu.cn>
2021-06-06 11:47:26 +0800

    finish over half of the RSA basic design

--- src/main/scala/Chainsaw/Crypto/RSA/RSADesign.scala
+++ src/main/scala/Chainsaw/Crypto/RSA/RSADesign.scala
@@ -31,7 +31,7 @@ case class MontExpInput(lN: Int) extends Bundle {
   val value = UInt(lN bits)
   val expononet = UInt(lN bits)
   val expononetLength = UInt(log2Up(lN) bits)
-  val modulus = UInt(lN bits)
+  val N = UInt(lN bits)
   val RhoSquare = UInt(lN bits)
   val omega = UInt(lN bits)
 }
@@ -44,19 +44,26 @@ class MontExp(lN: Int) extends DSPDUTTiming[MontExpInput, UInt] {
 
   // componnets
   val parameterRegs = Reg(MontExpInput(lN))
-  val doMult = RegInit(False)
+  val currentExponentBit = parameterRegs.expononet.msb
+
   val initDataRegs = Reg(UInt(lN bits)) // regs for "aMont"
+
   val onGoingDataRegs = Reg(UInt(2 * lN bits)) // regs for "reg"
   val onGoingHigh = onGoingDataRegs(2 * lN - 1 downto lN)
   val onGoingLow = onGoingDataRegs(lN - 1 downto 0)
+
+  val tempRegs = Reg(UInt(lN bits)) // regs for "reg"
+
   val mult = new BigMult(lN)
+
   val innerCounter = Counter(lN * 2)
-  //  val mult = new BigMultMod(lN)
 
   // datapath
   val op0 = UInt(lN bits)
   val op1 = UInt(lN bits)
-  val prod = UInt(lN bits)
+  val prod = UInt(2 * lN bits)
+  val prodHigh = prod(2 * lN - 1 downto lN)
+  val prodLow = (lN - 1 downto 0)
   mult.input(0) := op0
   mult.input(1) := op1
   prod := mult.output
@@ -69,22 +76,47 @@ class MontExp(lN: Int) extends DSPDUTTiming[MontExpInput, UInt] {
   op0 := U(0, lN bits)
   op1 := U(0, lN bits)
 
-  def montRedcOnData = {
+  def exponentMove() = parameterRegs.expononet := parameterRegs.expononet << 1
 
+  def montRedcOnData() = {
+    when(innerCounter.value === U(1)) { // second cycle, first mult of montRedc
+      op0 := onGoingLow
+      op1 := parameterRegs.omega
+      tempRegs := prodLow // U
+    }.elsewhen(innerCounter.value === U(2)) {
+      op0 := onGoingLow // U
+      op1 := parameterRegs.N // N
+      val mid = (prod + onGoingDataRegs) >> lN // (t + U * N) / Rho
+      onGoingLow := mid
+    }
+  }
+
+  def montMultOnData(input0: UInt, input1: UInt) = {
+    when(innerCounter.value === U(0)) { // first cycle, square
+      op0 := input0 // aMont ^ 2n / aMont ^ n
+      op1 := input1 // aMont / aMont ^ n
+      onGoingDataRegs := prod // aMont ^ (2n+1) / AMont ^ (2n)
+    }
+    montRedcOnData()
   }
 
   val fsm = new StateMachine {
     val INIT = new State() with EntryPoint
     val PRE = new State()
-    val DoSquareFor1 = new State()
-    val DoMultFor1 = new State()
-    val DoSquareFor0 = new State()
+    val DoSquareFor1 = new StateDelay(3)
+    val DoMultFor1 = new StateDelay(3)
+    val DoSquareFor0 = new StateDelay(3)
     val POST = new State()
 
     states.foreach(_.whenIsActive(innerCounter.increment()))
     states.foreach(_.onExit(innerCounter.clear()))
 
-    INIT.whenIsActive {
+    // state trasition
+    DoSquareFor1.whenCompleted(goto(DoMultFor1))
+    DoMultFor1.whenCompleted(goto(DoMultFor1))
+
+    // state workload
+    INIT.whenIsActive { // initialization
       parameterRegs := input
       goto(PRE)
     }
@@ -92,36 +124,27 @@ class MontExp(lN: Int) extends DSPDUTTiming[MontExpInput, UInt] {
       when(innerCounter.value === U(0)) { // first cycle
         op0 := parameterRegs.value // t
         op1 := parameterRegs.omega // omega
-        initDataRegs := prod(lN - 1 downto 0) // t * omega (mod Rho), named as U
+        initDataRegs := prodLow // t * omega (mod Rho), named as U
         // expose the second most significant bit where the SM sequence starts from
-        parameterRegs.expononet := parameterRegs.expononet << 1
+        exponentMove()
       }.otherwise { // second cycle
         op0 := initDataRegs // U
-        op1 := parameterRegs.modulus // N
+        op1 := parameterRegs.N // N
         // FIXME: an implicit 1024-bit adder, fix it
         // TODO: this should be part of the datapath
         val mid = (prod + parameterRegs.value) >> lN // (t + U * N) / Rho
         initDataRegs := mid
         onGoingLow := mid
-        when(parameterRegs.expononet.msb)(goto(DoSquareFor1))
+        // FIXME: add the reduction circuit
+        when(currentExponentBit)(goto(DoSquareFor1))
           .otherwise(goto(DoSquareFor0))
       }
     }
-    DoSquareFor1.whenIsActive {
-      when(innerCounter.value === U(0)) { // first cycle, square
-        op0 := onGoingDataRegs // aMont
-        op1 := onGoingDataRegs // aMont
-        onGoingDataRegs := prod // aMont ^ 2
-      }
-      when(innerCounter.value === U(1)) { // second cycle, first mult of montRedc
-        op0 := onGoingDataRegs // aMont ^ 2
-        op1 := parameterRegs.omega
-        onGoingDataRegs := prod
-      }.elsewhen(innerCounter.value === U(2)) {
-        op0 := onGoingDataRegs
-        op1 := onGoingDataRegs
-      }
-    }
+
+    DoSquareFor1.whenIsActive(montMultOnData(onGoingLow, onGoingLow))
+    DoMultFor1.whenIsActive(montMultOnData(onGoingLow, initDataRegs))
+    DoSquareFor0.whenIsActive(montMultOnData(onGoingLow, onGoingLow))
+
   }
 
 }
