d07a996735df6ddf16665c471a854becc0b612c0
ltr <litianr@mail2.sysu.edu.cn>
2021-12-03 21:49:57 +0800

    add breezeExample, showing the power of breeze

---
+++ src/main/scala/Chainsaw/core/FixedMath.scala
@@ -0,0 +1,9 @@
+package Chainsaw.core
+
+
+
+/** math strategy for fixed-point arithmetic, the default behavior is the same as verilog default(full precision)
+ *
+ * we use the strategies which has the same name as they are in the matlab fixed point designer
+ */
+case class FixedMath()
