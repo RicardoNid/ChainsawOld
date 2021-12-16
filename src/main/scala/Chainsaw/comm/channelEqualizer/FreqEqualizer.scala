24e5b148aadb946b132e4ff67ceae9517472cce6
ltr <litianr@mail2.sysu.edu.cn>
2021-12-16 16:46:48 +0800

    final version of equalization

--- src/main/scala/Chainsaw/comm/channelEqualizer/FreqEqualizer.scala
+++ src/main/scala/Chainsaw/comm/channelEqualizer/FreqEqualizer.scala
@@ -13,7 +13,7 @@ import scala.language.postfixOps
 case class VecAddSub(dspType: HardType[SFix], vecSize: Int) extends Component {
   val xs, ys = in(Vec(dspType(), vecSize))
   val mode = in Bits (1 bits)
-  mode.addAttribute("max_fanout", 32)
+  //  mode.addAttribute("max_fanout", 32)
   val rets = out(Vec(dspType(), vecSize))
 
   rets.zip(xs.zip(ys)).foreach { case (ret, (x, y)) => ret := Mux(mode(0), x + y, x - y) }
@@ -40,7 +40,11 @@ case class VecMult(dspType: HardType[SFix], vecSize: Int) extends Component {
   val xs, ys = in(Vec(dspType(), vecSize))
   val rets = out(Vec(dspType(), vecSize))
 
-  rets.zip(xs.zip(ys)).foreach { case (ret, (x, y)) => ret := (x * y).truncated }
+  rets.zip(xs.zip(ys)).foreach { case (ret, (x, y)) =>
+    val trunc = dspType()
+    trunc := (x * y).truncated
+    ret := RegNext(trunc)
+  }
 
   def drive(xOuter: Vec[SFix], yOuter: Vec[SFix], retOuter: Vec[SFix]) = {
     Seq(xOuter, yOuter).zip(Seq(xs, ys)).foreach { case (signal, port) =>
@@ -49,7 +53,7 @@ case class VecMult(dspType: HardType[SFix], vecSize: Int) extends Component {
     if (retOuter != null) retOuter := rets
   }
 
-  val mult: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = drive(_, _, _)
+  val mult: (Vec[SFix], Vec[SFix]) => Unit = drive(_, _, null)
 
   def idle() = drive(null, null, null)
 
@@ -60,11 +64,11 @@ case class VecMult(dspType: HardType[SFix], vecSize: Int) extends Component {
 
 }
 
-case class Equalization(dspType: HardType[SFix], vecSize:Int) extends Component {
+case class Equalization(dspType: HardType[SFix], vecSize: Int) extends Component {
 
   val complexType = HardType(ComplexNumber(dspType))
   val iteration = 12
-  val period = 2 * iteration + 3 * 16 + 3 + 2
+  val period = 2 * iteration + 3 * 16 + 3 + 2 + 1
   require(period <= 80)
 
   val preambleIn = slave Flow Vec(complexType, vecSize) // preambles after smooth
@@ -81,22 +85,25 @@ case class Equalization(dspType: HardType[SFix], vecSize:Int) extends Component 
   val dspTwo = Vec(SFLike(2.0, dspType()), vecSize)
   val dspQuarter = Vec(SFLike(0.25, dspType()), vecSize)
 
-  val tk, xk, temp0, temp1, temp2, temp3 = Reg(Vec(dspType(), vecSize)) // registers
+  val tk, xk, temp0, temp1, temp2 = Reg(Vec(dspType(), vecSize)) // registers
 
-  val dsp0, dsp1 = VecMult(dspType, vecSize)
-  val addSub0, addSub1 = VecAddSub(dspType, vecSize)
+  val dsps = Seq.fill(2)(VecMult(dspType, vecSize))
+  val Seq(dsp0, dsp1) = dsps
+  val Seq(prod0, prod1) = dsps.map(_.rets)
+  dsps.foreach(_.init())
 
-  Seq(addSub0, addSub1).foreach(_.init())
-  Seq(dsp0, dsp1).foreach(_.init())
+  val addSubs = Seq.fill(2)(VecAddSub(dspType, vecSize))
+  val Seq(addSub0, addSub1) = addSubs
+  addSubs.foreach(_.init())
 
-  dataOut.payload.zip(temp2.zip(temp3)).foreach { case (out, (real, imag)) =>
+  dataOut.payload.zip(temp1.zip(temp2)).foreach { case (out, (real, imag)) =>
     out.real := real
     out.imag := imag
   }
   dataOut.valid := RegNext(dataIn.valid)
 
-  val mult0: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = dsp0.mult
-  val mult1: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = dsp1.mult
+  val mult0: (Vec[SFix], Vec[SFix]) => Unit = dsp0.mult
+  val mult1: (Vec[SFix], Vec[SFix]) => Unit = dsp1.mult
   val add0: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = addSub0.add
   val add1: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = addSub1.add
   val sub0: (Vec[SFix], Vec[SFix], Vec[SFix]) => Unit = addSub0.sub
@@ -106,19 +113,19 @@ case class Equalization(dspType: HardType[SFix], vecSize:Int) extends Component 
   val counter = Counter(period)
   counter.value.simPublic()
 
-  val dbcCounter = Counter(2)
+  val dbcCounter = Counter(iteration * 2)
   dbcCounter.value.simPublic()
   val complexMultCounter = Counter(3)
   complexMultCounter.value.simPublic()
 
   val fsm = new StateMachine {
     val GETENERGY0 = StateEntryPoint()
-    val GETENERGY1, GETENERGY2, GETFACTOR0, GETFACTOR1 = new State()
+    val GETENERGY1, GETENERGY2, GETFACTOR0, GETFACTOR1, SAVE = new State()
     val DBC = new StateDelay(iteration * 2)
     val EQUALIZE = new StateDelay(48)
 
     //  state transition logic
-    val ordered = Seq(GETENERGY0, GETENERGY1, GETENERGY2, DBC, GETFACTOR0, GETFACTOR1, EQUALIZE)
+    val ordered = Seq(GETENERGY0, GETENERGY1, GETENERGY2, DBC, GETFACTOR0, GETFACTOR1, SAVE, EQUALIZE)
     ordered.zip(ordered.tail :+ GETENERGY0).foreach { case (prev, next) =>
       prev match {
         case delay: StateDelay => delay.whenCompleted(goto(next))
@@ -129,57 +136,65 @@ case class Equalization(dspType: HardType[SFix], vecSize:Int) extends Component 
 
     // state behavior
     GETENERGY0.whenIsActive {
-      mult0(preambleReal, preambleReal, tk) // real^2
-      mult1(preambleImag, preambleImag, xk) // imag^2
+      mult0(preambleReal, preambleReal) // real^2
+      mult1(preambleImag, preambleImag) // imag^2
       temp0 := preambleReal
       temp1 := preambleImag
     }
 
     GETENERGY1.whenIsActive {
-      add0(tk, xk, tk) // now, tk = energy
+      add0(prod0, prod1, tk) // now, tk = energy
       sub1(dspZero, temp1, temp1)
     }
 
     GETENERGY2.whenIsActive {
-      mult0(tk, dspQuarter, tk)
+      mult0(tk, dspQuarter) // prod0 = energy after norm
       xk := dspOne // now, xk = 1
     }
 
     DBC.whenIsActive { // towards 1 / energy
       dbcCounter.increment()
-      when(!dbcCounter.value.lsb)(sub0(dspTwo, tk, temp2))
-        .otherwise {
-          mult0(tk, temp2, tk)
-          mult1(xk, temp2, xk)
-        }
+      when(!dbcCounter.value.lsb) {
+        sub0(dspTwo, prod0, temp2)
+        tk := prod0
+        when(dbcCounter.value =/= U(0))(xk := prod1)
+      }.otherwise {
+        when(dbcCounter.value =/= U(iteration * 2 - 1)){
+          mult0(tk, temp2)
+        }.otherwise(mult0(xk, temp2))
+        mult1(xk, temp2)
+      }
     } // after this, xk = 1 / energy
 
     GETFACTOR0.whenIsActive {
-      mult0(xk, temp1, tk)
-      mult1(xk, temp0, xk)
+      mult0(prod0, temp1)
+      mult1(prod1, temp0)
     }
 
     GETFACTOR1.whenIsActive {
-      mult0(tk, dspQuarter, tk) // tk = factor.imag
-      mult1(xk, dspQuarter, xk) // xk = factor.real
+      mult0(prod0, dspQuarter) // tk = factor.imag
+      mult1(prod1, dspQuarter) // xk = factor.real
+    }
+
+    SAVE.whenIsActive {
+      tk := prod0
+      xk := prod1
     }
 
     EQUALIZE.whenIsActive { // data* factor, (a+bj) * (c+dj)
       complexMultCounter.increment()
       when(complexMultCounter.value === U(0)) {
-        mult0(xk, dataReal, temp0) // ac
-        mult1(tk, dataImag, temp1) // bd
-        temp2 := dataImag
-        temp3 := dataReal
+        mult0(tk, dataImag) // bd
+        mult1(xk, dataReal) // ac
+        temp0 := dataImag
+        temp1 := dataReal
+      }.elsewhen(complexMultCounter.value === U(1)) {
+        mult0(tk, temp1) // ab
+        mult1(xk, temp0) // bc
+        sub0(prod1, prod0, temp1) // result.real = ac - bd
+      }.otherwise {
+        add1(prod0, prod1, temp2) // result.imag
       }
-        .elsewhen(complexMultCounter.value === U(1)) {
-          mult0(xk, temp2, temp2) // bc
-          mult1(tk, temp3, temp3) // ab
-        }
-        .otherwise {
-          sub0(temp0, temp1, temp2) // result.real
-          add1(temp2, temp3, temp3) // result.imag
-        }
     }
   }
 }
@@ -194,7 +209,15 @@ case class FreqEqualizer(golden: Seq[Int], iteration: Int) extends Component {
 
 }
 
-object Equalization {
+object SynthBig extends App {
+  VivadoSynth(Equalization(HardType(SFix(7 exp, 18 bits)), 256), "EqualBig")
+}
+
+object SynthSmall extends App {
+  VivadoSynth(Equalization(HardType(SFix(7 exp, 18 bits)), 1), "EqualSmall")
+}
+
+object EqualizationTest {
   def main(args: Array[String]): Unit = {
 
     SimConfig.withWave.compile(Equalization(SFix(7 exp, 18 bits), 1)).doSim { dut =>
@@ -213,14 +236,12 @@ object Equalization {
         }
         dut.clockDomain.waitSampling()
       }
+      println(s"period: ${dut.period}")
       println(s"data: $dataIn")
       println(s"preamble: $preamble")
       println(dataIn / preamble)
       println(dutResult.head.head)
       assert(abs(dataIn / preamble - dutResult.head.head) < 1E-1)
     }
-
-    VivadoSynth(Equalization(HardType(SFix(7 exp, 18 bits)), 256))
-
   }
 }
